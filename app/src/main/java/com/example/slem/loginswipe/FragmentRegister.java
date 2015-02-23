ed1757ad7d933a6678eba6f195350fd6e05b215a
eslem <j.eslem03@gmail.com>
2015-02-23 02:10:47 +0100

    push

---
+++ app/src/main/java/com/example/slem/loginswipe/FragmentRegister.java
@@ -0,0 +1,25 @@
+package com.example.slem.loginswipe;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Slem on 23/02/2015.
+ */
+public class FragmentRegister extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        if (container == null) {
+            return null;
+        }
+
+        return (LinearLayout) inflater.inflate
+                (R.layout.fragment_register,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/slem/loginswipe/PageAdapter.java
@@ -0,0 +1,34 @@
+package com.example.slem.loginswipe;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Slem on 23/02/2015.
+ */
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public PageAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        // TODO Auto-generated constructor stub
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return this.fragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return this.fragments.size();
+    }
+
+}
